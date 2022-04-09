a26b1dfa34dc74b980b184da96927e05b719288a
kamranbhatti <bhattikj@gmail.com>
2022-04-08 23:21:16 -0400

    Added AfterMethod TearDown to FreeCrmTest

--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -26,6 +27,11 @@ public class FreeCrmTest {
 		driver = new ChromeDriver();
 		js = (JavascriptExecutor) driver;
 		driver.get("https://www.freecrm.com/index.html");
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
 	}
 
 	@Test
