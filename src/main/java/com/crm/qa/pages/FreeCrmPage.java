876e20b91104834b16f7e94c29c72f97d470e3c6
bhattikj-oh <bhattikj+oh@gmail.com>
2022-04-11 01:48:24 -0400

    FreeCrm test and page - used util logic

--- src/main/java/com/crm/qa/pages/FreeCrmPage.java
+++ src/main/java/com/crm/qa/pages/FreeCrmPage.java
@@ -1,10 +1,18 @@
 package com.crm.qa.pages;
 
+import java.io.IOException;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.support.PageFactory;
 
 import com.crm.qa.base.TestBase;
+import com.crm.qa.util.TestUtil;
 
 public class FreeCrmPage extends TestBase {
+
+	TestUtil testutil;
+	
+	
 
 	
 	public FreeCrmPage() { 
@@ -15,5 +23,38 @@ public class FreeCrmPage extends TestBase {
 		return driver.getTitle();
 	}
 	
+	public Boolean TitleMessageTest(String titleTest) throws InterruptedException {
+		Boolean result = false;
+		String title = this.FreeCrmTitleTest();
+		if (title.equals(titleTest)) {
+			try {
+		TestUtil.runTimeInfo("info", "title is correct!! YAY!!!");
+		result=true;
+		}
+			catch(InterruptedException ie) {
+				System.out.println("The thread was interrupted");
+			}
+			
+		}
+		else {
+			try {
+			TestUtil.runTimeInfo("error", "Title is not correct!! BUG BUG BUG!!!\"");
+			TestUtil.takeScreenshotAtEndOfTest();
+		}
+			catch(Exception e) {
+				System.out.println("The thread was interrupted");
+			}
+			result = false;
+		}
+		//	if (title.equals("Free CRM software in the cloud powers sales and customer serviceQQQQ")) {
+//		getRunTimeInfoMessage("info", "title is correct!! YAY!!!");
+//		Assert.assertTrue(true);
+//	} else {
+//		getRunTimeInfoMessage("error", "title is not correct!! BUG BUG BUG!!!");
+//		takeScreenshot("freecrmloginpage");
+//		Assert.assertTrue(false);
+//	}
+		return result;
+	}
 	
 }
--- src/test/java/com/crm/qa/testcases/FreeCrmTest.java
+++ src/test/java/com/crm/qa/testcases/FreeCrmTest.java
@@ -17,12 +17,15 @@ import org.testng.annotations.Test;
 
 import com.crm.qa.base.TestBase;
 import com.crm.qa.pages.FreeCrmPage;
+import com.crm.qa.util.TestUtil;
 
 public class FreeCrmTest extends TestBase{
 
 //	static WebDriver driver;   This is already created from the super 
-	static JavascriptExecutor js;
+//	static JavascriptExecutor js;
 	FreeCrmPage freeCrmPage;
+	
+
 
 	public FreeCrmTest() {
 		super();
@@ -33,6 +36,7 @@ public class FreeCrmTest extends TestBase{
 	public void setUp() throws Exception {
 		initialization();
 		freeCrmPage = new FreeCrmPage();
+		
 		
 		
 		// Driver is being inherited from super 
@@ -52,17 +56,21 @@ public class FreeCrmTest extends TestBase{
 
 	//Moving this to the page logic 
 	
-	public void freeCrmTitleTest() throws InterruptedException, IOException {
+	public void freeCrmTitleTest() {
 		
 		
 		String title = 	freeCrmPage.FreeCrmTitleTest();
 		Assert.assertEquals(title,"Free CRM software for customer relationship management, sales, marketing campaigns and support.");
 		
-//		String title = driver.getTitle();
-//		System.out.println("title is: " + title);
-		getRunTimeInfoMessage("info", title);
-
+	
+		//		String title = driver.getTitle();
+		//		System.out.println("title is: " + title);
+	}
 		
+	public void freeCrmTitleMessageTest() {
+		String message = "Free CRM software in the cloud powers sales and customer serviceQQQQ";
+		Assert.assertFalse(freeCrmPage.TitleMessageTest(message));
+	}
 		//none of this is a test
 //		if (title.equals("Free CRM software in the cloud powers sales and customer serviceQQQQ")) {
 //			getRunTimeInfoMessage("info", "title is correct!! YAY!!!");
@@ -74,41 +82,43 @@ public class FreeCrmTest extends TestBase{
 //		}
 
 	}
+	
+	//move this to the page logic
 
-	public static void getRunTimeInfoMessage(String messageType, String message) throws InterruptedException {
-		// Check for jQuery on the page, add it if need be
-		js.executeScript("if (!window.jQuery) {"
-				+ "var jquery = document.createElement('script'); jquery.type = 'text/javascript';"
-				+ "jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js';"
-				+ "document.getElementsByTagName('head')[0].appendChild(jquery);" + "}");
-		Thread.sleep(5000);
-
-		// Use jQuery to add jquery-growl to the page
-		js.executeScript("$.getScript('https://the-internet.herokuapp.com/js/vendor/jquery.growl.js')");
-
-		// js.executeScript("$.getScript('/Users/NaveenKhunteta/Documents/workspace/Test/src/testcases/jquery.growl.js')");
-
-		// Use jQuery to add jquery-growl styles to the page
-		js.executeScript("$('head').append('<link rel=\"stylesheet\" "
-				+ "href=\"https://the-internet.herokuapp.com/css/jquery.growl.css\" " + "type=\"text/css\" />');");
-		Thread.sleep(5000);
-
-		// jquery-growl w/ no frills
-		js.executeScript("$.growl({ title: 'GET', message: '/' });");
-		
-		if(messageType.equals("error")){
-			js.executeScript("$.growl.error({ title: 'ERROR', message: '"+message+"' });");
-		}else if(messageType.equals("info")){
-			js.executeScript("$.growl.notice({ title: 'Notice', message: '"+message+"' });");
-		}else if(messageType.equals("warning")){
-			js.executeScript("$.growl.warning({ title: 'Warning!', message: '"+message+"' });");
-		}
-
-		// jquery-growl w/ colorized output
-//		js.executeScript("$.growl.error({ title: 'ERROR', message: 'Some exception is coming' });");
-//		js.executeScript("$.growl.notice({ title: 'Notice', message: 'your notice message goes here' });");
-//		js.executeScript("$.growl.warning({ title: 'Warning!', message: 'your warning message goes here' });");
-	}
+//	public static void getRunTimeInfoMessage(String messageType, String message) throws InterruptedException {
+//		// Check for jQuery on the page, add it if need be
+//		js.executeScript("if (!window.jQuery) {"
+//				+ "var jquery = document.createElement('script'); jquery.type = 'text/javascript';"
+//				+ "jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js';"
+//				+ "document.getElementsByTagName('head')[0].appendChild(jquery);" + "}");
+//		Thread.sleep(5000);
+//
+//		// Use jQuery to add jquery-growl to the page
+//		js.executeScript("$.getScript('https://the-internet.herokuapp.com/js/vendor/jquery.growl.js')");
+//
+//		// js.executeScript("$.getScript('/Users/NaveenKhunteta/Documents/workspace/Test/src/testcases/jquery.growl.js')");
+//
+//		// Use jQuery to add jquery-growl styles to the page
+//		js.executeScript("$('head').append('<link rel=\"stylesheet\" "
+//				+ "href=\"https://the-internet.herokuapp.com/css/jquery.growl.css\" " + "type=\"text/css\" />');");
+//		Thread.sleep(5000);
+//
+//		// jquery-growl w/ no frills
+//		js.executeScript("$.growl({ title: 'GET', message: '/' });");
+//		
+//		if(messageType.equals("error")){
+//			js.executeScript("$.growl.error({ title: 'ERROR', message: '"+message+"' });");
+//		}else if(messageType.equals("info")){
+//			js.executeScript("$.growl.notice({ title: 'Notice', message: '"+message+"' });");
+//		}else if(messageType.equals("warning")){
+//			js.executeScript("$.growl.warning({ title: 'Warning!', message: '"+message+"' });");
+//		}
+//
+//		// jquery-growl w/ colorized output
+////		js.executeScript("$.growl.error({ title: 'ERROR', message: 'Some exception is coming' });");
+////		js.executeScript("$.growl.notice({ title: 'Notice', message: 'your notice message goes here' });");
+////		js.executeScript("$.growl.warning({ title: 'Warning!', message: 'your warning message goes here' });");
+//	}
 	
 //This will be added as a method to the testBase in case any other Tests wish to implement it.
 //	public static void takeScreenshot(String fileName) throws IOException{
