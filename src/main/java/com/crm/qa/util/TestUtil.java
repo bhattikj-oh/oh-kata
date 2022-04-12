92c960b1a72fec3ee5603a1fcdf09ba210c25d02
bhattikj-oh <bhattikj+oh@gmail.com>
2022-04-12 18:14:35 -0400

    Increased timeout -- TestUtil

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -18,8 +18,8 @@ import com.crm.qa.base.TestBase;
 
 public class TestUtil extends TestBase {
 
-	public static long PAGE_LOAD_TIMEOUT = 20;
-	public static long IMPLICIT_WAIT = 20;
+	public static long PAGE_LOAD_TIMEOUT = 440;
+	public static long IMPLICIT_WAIT = 440;
 
 	public static String TESTDATA_SHEET_PATH = "src/main/java/com/crm/qa/testdata/FreeCrmTestData.xlsx";
 
